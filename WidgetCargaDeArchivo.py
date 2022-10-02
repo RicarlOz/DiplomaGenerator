1e07921ea1131a70abc27719ac9021c6e58beb29
STapia24 <tapia24@hotmail.com>
2022-10-01 23:01:44 -0500

    Pantalla Carga de Archivo

---
+++ WidgetCargaDeArchivo.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTextEdit, QPushButton
+from PyQt5 import uic
+
+# Clase heredada de QMainWindow (Constructor de ventanas)
+class window(QMainWindow):
+    def __init__(self):
+        # Iniciar el objeto QMainWindow
+        QMainWindow.__init__(self)
+        # Cargar la config del archivo .ui en el objeto
+        uic.loadUi("WidgetCargaDeArchivo.ui", self)
+        
+
+# Instancia para iniciar una aplicacion
+app = QApplication(sys.argv)
+
+# Crear el objeto de la clase
+_window = window()
+
+# Mostrar la ventana
+_window.show()
+
+# Ejecutar la app
+app.exec()
