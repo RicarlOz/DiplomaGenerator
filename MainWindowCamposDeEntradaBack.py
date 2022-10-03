8f29735f4bfcdd9204fe9bc8f894c9477e661232
Ricardo Gomez <A01235366@itesm.mx>
2022-10-02 23:55:50 -0500

    Executable for sprint 1

--- MainWindowCamposDeEntradaBack.py
+++ MainWindowCamposDeEntradaBack.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLineEdit, QPushButton, QDialog
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
 
 nombreTaller = 'NA'
@@ -10,6 +11,7 @@ class window(QMainWindow):
     def __init__(self):
         # Iniciar el objeto QMainWindow
         QMainWindow.__init__(self)
+        # super(window, self).__init__()
 
         # Cargar la config del archivo .ui en el objeto
         uic.loadUi("MainWindowCamposDeEntrada.ui", self)
@@ -27,16 +29,52 @@ class window(QMainWindow):
         nombreTaller = self.lineEditTaller.text()
         fechaTaller = self.lineEditFecha.text()
         print("El nombre del taller es " + nombreTaller + " y su fecha es " + fechaTaller)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class Ui_Form(QDialog):
+    def __init__(self):
+        super(Ui_Form, self).__init__()
+        uic.loadUi("WidgetCargaDeArchivo.ui", self)
+        self.pushButton.clicked.connect(self.pushButton_handler)
+        self.pushButton_4.clicked.connect(self.pushButton_handler_4)
+
+    def pushButton_handler(self):
+        print("Button pressed")
+        self.open_dialog_box()
+        
+    def open_dialog_box(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName()
+        path = filename[0]
+        self.label.setText(path)
+        print(path)
+
+    def pushButton_handler_4(self):
+        print("Button pressed")
+        self.open_dialog_box_4()
+        
+    def open_dialog_box_4(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName()
+        path = filename[0]
+        self.label_3.setText(path)
+        print(path)
         
 
 # Instancia para iniciar una aplicacion
 app = QApplication(sys.argv)
 
+widget = QtWidgets.QStackedWidget()
+
 # Crear el objeto de la clase
 _window = window()
 
+screen2 = Ui_Form()
+
+widget.addWidget(_window)
+widget.addWidget(screen2)
+
 # Mostrar la ventana
-_window.show()
+# _window.show()
+widget.show()
 
 # Ejecutar la app
 app.exec()
