c9c99311fe1ccc8fefd203f05dc7feead1d76af1
RicarlOz <31571772+RicarlOz@users.noreply.github.com>
2022-10-21 00:03:07 -0500

    App generating Diplomas

--- DiplomaGenerator.py
+++ DiplomaGenerator.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QFileDialog, QStackedWidget, QVBoxLayout, QLineEdit, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QApplication, QDialog, QGridLayout, QMessageBox, QFileDialog, QStackedWidget, QVBoxLayout, QLineEdit, QPushButton, QTextEdit
 from PyQt5 import uic, QtWebEngineWidgets, QtCore
 from fpdf import FPDF
 # import PIL.Image as Image
@@ -9,11 +9,11 @@ import time
 
 nombreTaller = 'NA'
 fechaTaller = 'NA'
-selectedTemplate = None
+diplomaDescription = ''
+templateDesign = None
+selectedImage = None
 df = None
 
-
-# Clase heredada de QMainWindow (Constructor de ventanas)
 class DiplomaFields(QDialog):
     def __init__(self):
         super(DiplomaFields, self).__init__()
@@ -25,16 +25,56 @@ class DiplomaFields(QDialog):
         self.tfName = self.findChild(QLineEdit, "tfName")
         self.tfDate = self.findChild(QLineEdit, "tfDate")
         self.btnSubmit = self.findChild(QPushButton, "btnSubmit")
+        self.tfDescription = self.findChild(QTextEdit, "tfDescription")
 
         # Evento de Boton
         self.btnSubmit.clicked.connect(self.submit)
     
     def submit(self):
+        global diplomaDescription, fechaTaller, nombreTaller
+
         # Guarda la informacion de taller y su fecha
         nombreTaller = self.tfName.text()
         fechaTaller = self.tfDate.text()
-        descripcion = self.txtDescripcion.toPlainText()
-        print("El nombre del taller es " + nombreTaller + "la descripcion es " + descripcion + "y su fecha es " + fechaTaller)
+        diplomaDescription = self.tfDescription.toPlainText()
+        print("El nombre del taller es " + nombreTaller + "la descripcion es " + diplomaDescription + "y su fecha es " + fechaTaller)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class SeleccionTemplate(QDialog):
+    def __init__(self):
+        super(SeleccionTemplate, self).__init__()
+
+        # Cargar la config del archivo .ui en el objeto
+        uic.loadUi("SeleccionTemplate.ui", self)
+
+        # Definir Widgets
+        self.btnLeft = self.findChild(QPushButton, "btnLeft")
+        self.btnRight = self.findChild(QPushButton, "btnRight")
+        self.btnCenter = self.findChild(QPushButton, "btnCenter")
+        self.btnNext = self.findChild(QPushButton, "btnNext")
+        self.btnBack = self.findChild(QPushButton, "btnBack")
+
+        # Evento de Boton
+        self.btnLeft.clicked.connect(lambda: self.selectTemplate('L'))
+        self.btnRight.clicked.connect(lambda: self.selectTemplate('R'))
+        self.btnCenter.clicked.connect(lambda: self.selectTemplate('C'))
+        self.btnNext.clicked.connect(self.goNext)
+        self.btnBack.clicked.connect(self.goBack)
+
+    def selectTemplate(self, templateSelected):
+        global templateDesign
+        templateDesign = templateSelected
+
+    def goBack(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+        
+    def goNext(self):
+        global templateDesign
+
+        if templateDesign == None:
+            QMessageBox.warning(self, "Diseño no seleccionado.", "Selecciona un diseño base para continuar.")
+            return
+        print("Selected template:", templateDesign)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 class FileUpload(QDialog):
@@ -47,7 +87,9 @@ class FileUpload(QDialog):
         self.btnPreviewTemplate.clicked.connect(self.previewTemplate)
 
     def selectImg(self):
-        file = QFileDialog.getOpenFileName()
+        global selectedImage
+
+        file = QFileDialog.getOpenFileName(filter="Imagenes(*.png *.jpg *.jpeg)")
         path = file[0]
         file_extension = os.path.splitext(path)[1]
 
@@ -58,6 +100,8 @@ class FileUpload(QDialog):
         if file_extension not in [".png", ".jpg", ".jpeg"]:
             QMessageBox.warning(self, "Archivo no soportado", "Los archivos soportados son: .png, .jpg y .jpeg.")
             return
+
+        selectedImage = path
             
         fileName = path.split('/')[-1]
         self.lbFileNameImg.setText(fileName)
@@ -66,7 +110,7 @@ class FileUpload(QDialog):
     def selectExcel(self):
         global df
 
-        file = QFileDialog.getOpenFileName()
+        file = QFileDialog.getOpenFileName(filter="Hoja de calculo(*.xlsx *.xls *.csv)")
         path = file[0]
         file_extension = os.path.splitext(path)[1]
 
@@ -95,26 +139,75 @@ class FileUpload(QDialog):
         
     def previewTemplate(self):
         self.createPDF()
-        if len(widget.children()) <= 3:
-            screen3 = PreviewDiploma()
-            widget.addWidget(screen3)
+        print(len(widget.children()))
+        if len(widget.children()) <= 4:
+            screen4 = PreviewDiploma()
+            widget.addWidget(screen4)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
     def createPDF(self):
+        global templateDesign, diplomaDescription, fechaTaller, df
+
         #Creates the PDF document
         pdf = FPDF('L', 'mm', 'Letter')
 
         #Set margins
         pdf.set_margins(left=0, top=0, right=0)
 
+        #Disable auto page break
+        pdf.set_auto_page_break(False)
+
         #Add a page
         pdf.add_page()
 
-        pdf.image("Design/design1.jpg", 0, 0, 279.4, 215.9)
+        for idx, row in df.iterrows():
+            pdf.image(selectedImage, 0, 0, 279.4, 215.9)
 
-        pdf.set_xy(22, 76)
-        pdf.set_font('Arial', 'B', 16)
-        pdf.cell(165, 10, txt="RICARDO SERGIO GOMEZ CARDENAS", border=True, align='L')
+            ## Left
+            if templateDesign == 'L':
+                pdf.set_font('Arial', 'B', 18)
+                pdf.set_xy(24, 82)
+                pdf.cell(165, 10, txt=row["Nombre"], border=True, align='L')
+
+                pdf.set_font('Arial', '', 14)
+                pdf.set_xy(24, 100)
+                pdf.multi_cell(165, 5, txt=diplomaDescription, border=True, align='L')
+
+                pdf.set_font('Arial', '', 14)
+                pdf.set_xy(24, 150)
+                pdf.cell(85, 15, txt=fechaTaller, border=True, align='L')
+                
+            ## Right
+            elif templateDesign == 'R':
+                pdf.set_font('Arial', 'B', 18)
+                pdf.set_xy(92, 82)
+                pdf.cell(165, 10, txt=row["Nombre"], border=True, align='R')
+
+                pdf.set_font('Arial', '', 14)
+                pdf.set_xy(92, 100)
+                pdf.multi_cell(165, 5, txt=diplomaDescription, border=True, align='R')
+
+                pdf.set_font('Arial', '', 14)
+                pdf.set_xy(172, 150)
+                pdf.cell(85, 15, txt=fechaTaller, border=True, align='R')
+
+            ## Center
+            else:
+                width = 170
+                pdf.set_font('Arial', 'B', 18)
+                pdf.set_xy((279.4 / 2 - width / 2), 95)
+                pdf.cell(width, 10, txt=row["Nombre"], border=True, align='C')
+
+                pdf.set_font('Arial', '', 14)
+                pdf.set_xy((279.4 / 2 - width / 2), 110)
+                pdf.multi_cell(width, 5, txt=diplomaDescription, border=True, align='C')
+
+                pdf.set_font('Arial', '', 14)
+                pdf.set_xy(180, 188)
+                pdf.cell(85, 15, txt=fechaTaller, border=True, align='C')
+            
+            if idx < len(df) - 1:
+                pdf.add_page()
 
         pdf.output("diplomas.pdf")
 
@@ -153,10 +246,12 @@ widget = QStackedWidget()
 
 # Crear el objeto de la clase
 screen1 = DiplomaFields()
-screen2 = FileUpload()
+screen2 = SeleccionTemplate()
+screen3 = FileUpload()
 
 widget.addWidget(screen1)
 widget.addWidget(screen2)
+widget.addWidget(screen3)
 
 # Mostrar la ventana
 # _window.show()
