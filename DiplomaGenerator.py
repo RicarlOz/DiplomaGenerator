441a627a27f2e51d075fc86048f60f2c5bf8efda
RicarlOz <31571772+RicarlOz@users.noreply.github.com>
2022-11-02 15:45:54 -0600

    Files moved to folders, updated screens

--- DiplomaGenerator.py
+++ DiplomaGenerator.py
@@ -1,27 +1,28 @@
 from PyQt5.QtWidgets import QApplication, QDialog, QRadioButton, QMessageBox, QFileDialog, QStackedWidget, QVBoxLayout, QLineEdit, QPushButton, QTextEdit, QFontComboBox
 from PyQt5 import uic, QtWebEngineWidgets, QtCore
 from fpdf import FPDF
-# import PIL.Image as Image
 import pandas as pd
 import os
 import sys
-import time
+import pprint
 
-nombreTaller = 'NA'
-fechaTaller = 'NA'
+nombreTaller = None
+fechaTaller = None
+diplomaDescription = None
 asuntoCorreo = 'NA'
 cuerpoCorreo = 'NA'
-diplomaDescription = ''
 templateDesign = None
 selectedImage = None
+selectedData = None
 df = None
+screens = []
 
 class DiplomaFields(QDialog):
     def __init__(self):
         super(DiplomaFields, self).__init__()
 
         # Cargar la config del archivo .ui en el objeto
-        uic.loadUi("DiplomaFieldsScreen.ui", self)
+        uic.loadUi("ui/DiplomaFieldsScreen.ui", self)
 
         # Definir Widgets
         self.tfName = self.findChild(QLineEdit, "tfName")
@@ -33,11 +34,17 @@ class DiplomaFields(QDialog):
         self.btnSubmit.clicked.connect(self.submit)
     
     def submit(self):
+        global diplomaDescription, fechaTaller, nombreTaller
         # Guarda la informacion de taller y su fecha
         nombreTaller = self.tfName.text()
         fechaTaller = self.tfDate.text()
         diplomaDescription = self.tfDescription.toPlainText()
-        print("El nombre del taller es " + nombreTaller + " la descripcion es " + diplomaDescription + " y su fecha es " + fechaTaller)
+
+        # if nombreTaller == '' or fechaTaller == '' or diplomaDescription == '':
+        #     QMessageBox.warning(self, "Campos restantes.", "Llena la información de los campos para continuar.")
+        #     return
+
+        print("El nombre del taller es " + nombreTaller + "la descripcion es " + diplomaDescription + "y su fecha es " + fechaTaller)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 class SeleccionTemplate(QDialog):
@@ -45,7 +52,7 @@ class SeleccionTemplate(QDialog):
         super(SeleccionTemplate, self).__init__()
 
         # Cargar la config del archivo .ui en el objeto
-        uic.loadUi("SeleccionTemplate.ui", self)
+        uic.loadUi("ui/SeleccionTemplate.ui", self)
 
         # Definir Widgets
         self.btnLeft = self.findChild(QPushButton, "btnLeft")
@@ -63,9 +70,6 @@ class SeleccionTemplate(QDialog):
         self.btnLeft.clicked.connect(lambda: self.selectTemplate('L'))
         self.btnRight.clicked.connect(lambda: self.selectTemplate('R'))
         self.btnCenter.clicked.connect(lambda: self.selectTemplate('C'))
-        # self.rbLeft.toggled.connect(lambda: self.selectTemplate('L'))
-        # self.rbRight.toggled.connect(lambda: self.selectTemplate('R'))
-        # self.rbCenter.toggled.connect(lambda: self.selectTemplate('C'))
         self.btnNext.clicked.connect(self.goNext)
         self.btnBack.clicked.connect(self.goBack)
 
@@ -96,7 +100,7 @@ class SeleccionTemplate(QDialog):
 class FileUpload(QDialog):
     def __init__(self):
         super(FileUpload, self).__init__()
-        uic.loadUi("FileUploadScreen.ui", self)
+        uic.loadUi("ui/FileUploadScreen.ui", self)
         self.btnChooseFileImg.clicked.connect(self.selectImg)
         self.btnChooseFileExcel.clicked.connect(self.selectExcel)
         self.btnGoBack.clicked.connect(self.goBack)
@@ -124,7 +128,7 @@ class FileUpload(QDialog):
         print(path)
 
     def selectExcel(self):
-        global df
+        global selectedData, df
 
         file = QFileDialog.getOpenFileName(filter="*.xlsx *.xls *.csv")
         path = file[0]
@@ -137,6 +141,8 @@ class FileUpload(QDialog):
         if file_extension not in [".xlsx", ".xls", ".csv"]:
             QMessageBox.warning(self, "Archivo no soportado.", "Los archivos soportados son: .xlsx, .xls y .csv.")
             return
+
+        selectedData = path
 
         fileName = path.split('/')[-1]
         self.lbFileNameExcel.setText(fileName)
@@ -154,7 +160,19 @@ class FileUpload(QDialog):
         widget.setCurrentIndex(widget.currentIndex()-1)
         
     def previewTemplate(self):
+        global selectedImage, selectedData, screens
+
+        if selectedImage == None or selectedData == None:
+            QMessageBox.warning(self, "Archivo no seleccionado.", "Selecciona los archivos para continuar.")
+            return
+        
         self.createPDF()
+        pprint.pprint(widget.children())
+        if len(screens) <= 3:
+            screen4 = PreviewDiploma()
+            screens.append(screen4)
+            widget.addWidget(screen4)
+        screens[3].reloadPDF()
         widget.setCurrentIndex(widget.currentIndex()+1)
 
     def createPDF(self):
@@ -232,56 +250,101 @@ class FileUpload(QDialog):
 class PreviewDiploma(QDialog):
     def __init__(self):
         super(PreviewDiploma, self).__init__()
-        uic.loadUi("PreviewDiploma.ui", self)
+        uic.loadUi("ui/PreviewDiploma.ui", self)
 
         # Definir Widgets
         self.btnBack = self.findChild(QPushButton, "btnBack")
         self.btnNext = self.findChild(QPushButton, "btnNext")
         self.pdfLayout = self.findChild(QVBoxLayout, "vlPDF")
 
-        pdfViewer = QtWebEngineWidgets.QWebEngineView()
-        self.pdfLayout.addWidget(pdfViewer)
-        # pdfViewer.setGeometry()
-        pdfViewer.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
-        pdfViewer.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PdfViewerEnabled, True)
+        self.pdfViewer = QtWebEngineWidgets.QWebEngineView()
+        self.pdfLayout.addWidget(self.pdfViewer)
+        self.pdfViewer.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
+        self.pdfViewer.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PdfViewerEnabled, True)
 
         path = os.path.join(os.path.abspath(os.getcwd()), "diplomas.pdf")
         print(path)
-        pdfViewer.load(QtCore.QUrl.fromUserInput(path))
-        pdfViewer.reload()
+        self.pdfViewer.load(QtCore.QUrl.fromUserInput(path))
+        self.pdfViewer.reload()
 
         self.btnBack.clicked.connect(self.goBack)
-        self.btnNext.clicked.connect(self.sendMail)
+        self.btnNext.clicked.connect(self.goNext)
 
+    def reloadPDF(self):
+        self.pdfViewer.reload()
 
     def goBack(self):
         print("Back to screen 1")
+        # pprint.pprint(widget.children())
+        # widget.removeWidget(screens[3])
+        # screens[3].deleteLater()
+        pprint.pprint(widget.children())
         widget.setCurrentIndex(widget.currentIndex()-1)
 
-    def sendMail(self):
+    def goNext(self):
+        print(len(screens))
+        if len(screens) <= 4:
+            screen5 = SendMailsQuestion()
+            screens.append(screen5)
+            widget.addWidget(screen5)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
-##tfName, tfDescription, btnSubmit
-class SendMail(QDialog):
+class SendMailsQuestion(QDialog):
     def __init__(self):
-        super(SendMail, self).__init__()
-        uic.loadUi("EmailScreen.ui", self)
+        super(SendMailsQuestion, self).__init__()
 
+        # Cargar la config del archivo .ui en el objeto
+        uic.loadUi("ui/SendMailsQuestion.ui", self)
+
+        # Definir Widgets
+        self.btnYes = self.findChild(QPushButton, "btnYes")
+        self.btnNo = self.findChild(QPushButton, "btnNo")
+        self.btnBack = self.findChild(QPushButton, "btnBack")
+
+        # Evento de botón
+        self.btnBack.clicked.connect(self.goBack)
+        self.btnYes.clicked.connect(lambda: self.selectOption(True))
+        self.btnNo.clicked.connect(lambda: self.selectOption(False))
+
+    def selectOption(self, sendMail):
+        if sendMail:
+            if len(widget.children()) <= 6:
+                screen6 = MailFields()
+                widget.addWidget(screen6)
+            widget.setCurrentIndex(widget.currentIndex()+1)
+        else:
+            pass
+
+    def goBack(self):
+        screens[3].reloadPDF()
+        widget.setCurrentIndex(widget.currentIndex()-1)
+
+class MailFields(QDialog):
+    def __init__(self):
+        super(MailFields, self).__init__()
+
+        # Cargar la config del archivo .ui en el objeto
+        uic.loadUi("ui/MailFields.ui", self)
+
+        # Definir Widgets
+        self.btnBack = self.findChild(QPushButton, "btnBack")
+        self.btnNext = self.findChild(QPushButton, "btnNext")
         self.tfAsunto = self.findChild(QLineEdit, "tfSubject")
         self.tfContenido = self.findChild(QTextEdit, "tfBody")
         self.btnSubmit = self.findChild(QPushButton, "btnSubmit")
 
-        self.btnSubmit.clicked.connect(self.submit)
-
+        # Evento de botón
+        self.btnBack.clicked.connect(self.goBack)
+        
+    def goBack(self):
+        widget.setCurrentIndex(widget.currentIndex()-1)
+    
     def submit(self):
         asuntoCorreo = self.tfAsunto.text()
         cuerpoCorreo = self.tfContenido.toPlainText()
         print("El asunto del correo es: " + asuntoCorreo + " el contenido del correo es: " + cuerpoCorreo)
         print("Mostrar siguiente pantalla")
         # widget.setCurrentIndex(widget.currentIndex()+1)
-
-
-
 
 # Instancia para iniciar una aplicacion
 app = QApplication(sys.argv)
@@ -292,14 +355,15 @@ widget = QStackedWidget()
 screen1 = DiplomaFields()
 screen2 = SeleccionTemplate()
 screen3 = FileUpload()
-screen4 = PreviewDiploma()
-screen5 = SendMail()
+# screen4 = PreviewDiploma()
+# screen5 = SendMail()
 
-widget.addWidget(screen1)
-widget.addWidget(screen2)
-widget.addWidget(screen3)
-widget.addWidget(screen4)
-widget.addWidget(screen5)
+screens.append(screen1)
+screens.append(screen2)
+screens.append(screen3)
+
+for screen in screens:
+    widget.addWidget(screen)
 
 # Mostrar la ventana
 # _window.show()
