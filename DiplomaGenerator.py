b591f85dcb3e6576fd95817dfc1b91ed60b12f00
RicarlOz <31571772+RicarlOz@users.noreply.github.com>
2022-11-03 17:36:05 -0600

    CBox updated to available fonts, add font function added

--- DiplomaGenerator.py
+++ DiplomaGenerator.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import sys
 import pprint
+import shutil
 
 nombreTaller = None
 fechaTaller = None
@@ -14,10 +15,12 @@ cuerpoCorreo = None
 templateDesign = None
 selectedImage = None
 selectedData = None
+selectedFont = None
 df = None
+libraryFonts = ['Arial', 'Courier', 'Helvetica', 'Symbol', 'Times', 'ZapfDingbats']
 screens = []
 
-fontText = None
+selectedFont = None
 fontColor = None
 
 class DiplomaFields(QDialog):
@@ -53,6 +56,7 @@ class DiplomaFields(QDialog):
 class SeleccionTemplate(QDialog):
     def __init__(self):
         super(SeleccionTemplate, self).__init__()
+        global libraryFonts
 
         # Cargar la config del archivo .ui en el objeto
         uic.loadUi("ui/SeleccionTemplate.ui", self)
@@ -67,13 +71,27 @@ class SeleccionTemplate(QDialog):
         self.btnNext = self.findChild(QPushButton, "btnNext")
         self.btnBack = self.findChild(QPushButton, "btnBack")
         self.btnColor = self.findChild(QPushButton, "btnColor")
-        self.fontBox = self.findChild(QFontComboBox, "fontBox")
+        self.btnAddFont = self.findChild(QPushButton, "btnAddFont")
+        self.cbFont = self.findChild(QComboBox, "cbFont")
+
+        self.availableFonts = libraryFonts
+
+        if not os.path.exists("./fonts/"):
+            os.makedirs("./fonts/")
+        
+        for font in os.listdir("./fonts"):
+            if font[-4:] == ".ttf":
+                self.availableFonts.append(font[:-4])
+
+        self.availableFonts.sort()
+        self.cbFont.addItems(self.availableFonts)
 
         # Evento de Boton
         self.btnLeft.clicked.connect(lambda: self.selectTemplate('L'))
         self.btnRight.clicked.connect(lambda: self.selectTemplate('R'))
         self.btnCenter.clicked.connect(lambda: self.selectTemplate('C'))
         self.btnColor.clicked.connect(self.selectColor)
+        self.btnAddFont.clicked.connect(self.addFont)
         self.btnNext.clicked.connect(self.goNext)
         self.btnBack.clicked.connect(self.goBack)
 
@@ -96,17 +114,46 @@ class SeleccionTemplate(QDialog):
             print(fontColor.name())
             print(fontColor.getRgb())
 
+    def addFont(self):
+        global selectedFont, avaliab
+
+        file = QFileDialog.getOpenFileName(filter="*.ttf")
+        path = file[0]
+        file_extension = os.path.splitext(path)[1]
+
+        if path == "":
+            print("File not chosen.")
+            return
+
+        if file_extension not in [".ttf"]:
+            QMessageBox.warning(self, "Archivo no soportado", "Los archivos soportados son: .ttf")
+            return
+
+        fileName = path.split('/')[-1]
+
+        if not os.path.isfile("./fonts/" + fileName):
+            shutil.copy(path, "./fonts/")
+
+            self.availableFonts.append(fileName[:-4])
+            self.availableFonts.sort()
+
+            # Reaload combobox
+            self.cbFont.clear()
+            self.addItems(self.availableFonts)
+
+        self.cbFont.setCurrentIndex(self.availableFonts.index(fileName[:-4]))
+
     def goBack(self):
         widget.setCurrentIndex(widget.currentIndex()-1)
         
     def goNext(self):
-        global templateDesign, fontText
-        fontText = self.fontBox.currentText()
+        global templateDesign, selectedFont
+        selectedFont = self.cbFont.currentText()
         if templateDesign == None:
             QMessageBox.warning(self, "Diseño no seleccionado.", "Selecciona un diseño base para continuar.")
             return
         print("Selected template:", templateDesign)
-        print("El font elegido para el diploma es: " + fontText)
+        print("El font elegido para el diploma es: " + selectedFont)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
 class FileUpload(QDialog):
@@ -142,7 +189,7 @@ class FileUpload(QDialog):
     def selectExcel(self):
         global selectedData, df
 
-        file = QFileDialog.getOpenFileName(filter="*.xlsx *.xls *.csv")
+        file = QFileDialog.getOpenFileName(filter="*.xlsx *.xls")
         path = file[0]
         file_extension = os.path.splitext(path)[1]
 
@@ -150,8 +197,8 @@ class FileUpload(QDialog):
             print("File not chosen.")
             return
 
-        if file_extension not in [".xlsx", ".xls", ".csv"]:
-            QMessageBox.warning(self, "Archivo no soportado.", "Los archivos soportados son: .xlsx, .xls y .csv.")
+        if file_extension not in [".xlsx", ".xls"]:
+            QMessageBox.warning(self, "Archivo no soportado.", "Los archivos soportados son: .xlsx y .xls.")
             return
 
         selectedData = path
@@ -188,8 +235,8 @@ class FileUpload(QDialog):
         widget.setCurrentIndex(widget.currentIndex()+1)
 
     def createPDF(self):
-        global templateDesign, diplomaDescription, fechaTaller, df, fontText
-        print(fontText)
+        global templateDesign, diplomaDescription, fechaTaller, df, selectedFont
+        print(selectedFont)
 
         #Creates the PDF document
         pdf = FPDF('L', 'mm', 'Letter')
@@ -204,38 +251,38 @@ class FileUpload(QDialog):
         pdf.add_page()
 
         #Add font
-        # pdf.add_font(fontText, "", 'C:\Windows\Fonts\\' + fontText + '.ttf')
+        # pdf.add_font(selectedFont, "", 'C:\Windows\Fonts\\' + selectedFont + '.ttf')
 
         for idx, row in df.iterrows():
             pdf.image(selectedImage, 0, 0, 279.4, 215.9)
 
             ## Left
             if templateDesign == 'L':
-                pdf.set_font(fontText, 'B', 18)
+                pdf.set_font(selectedFont, 'B', 18)
                 pdf.set_xy(24, 82)
                 pdf.cell(165, 10, txt=row["Nombre"], border=True, align='L')
 
-                pdf.set_font(fontText, '', 14)
+                pdf.set_font(selectedFont, '', 14)
                 pdf.set_xy(24, 100)
                 pdf.multi_cell(165, 5, txt=diplomaDescription, border=True, align='L')
 
-                pdf.set_font(fontText, '', 14)
+                pdf.set_font(selectedFont, '', 14)
                 pdf.set_xy(24, 150)
                 pdf.cell(85, 15, txt=fechaTaller, border=True, align='L')
                 
             ## Right
             elif templateDesign == 'R':
-                pdf.set_font(fontText, 'B', 25)
+                pdf.set_font(selectedFont, 'B', 25)
                 pdf.set_text_color(241, 194, 50)
                 pdf.set_xy(92 - 25, 82)
                 pdf.cell(165, 10, txt=row["Nombre"], border=True, align='R')
 
-                pdf.set_font(fontText, '', 14)
+                pdf.set_font(selectedFont, '', 14)
                 pdf.set_text_color(255, 255, 255)
                 pdf.set_xy(92 - 25, 100)
                 pdf.multi_cell(165, 5, txt=diplomaDescription, border=True, align='R')
 
-                pdf.set_font(fontText, '', 14)
+                pdf.set_font(selectedFont, '', 14)
                 pdf.set_text_color(255, 255, 255)
                 pdf.set_xy(172 - 25, 150)
                 pdf.cell(85, 15, txt=fechaTaller, border=True, align='R')
@@ -243,15 +290,15 @@ class FileUpload(QDialog):
             ## Center
             else:
                 width = 170
-                pdf.set_font(fontText, 'B', 18)
+                pdf.set_font(selectedFont, 'B', 18)
                 pdf.set_xy((279.4 / 2 - width / 2) + 10, 120)
                 pdf.cell(width, 10, txt=row["Nombre"], border=False, align='C')
 
-                pdf.set_font(fontText, '', 14)
+                pdf.set_font(selectedFont, '', 14)
                 pdf.set_xy((279.4 / 2 - width / 2) + 10, 135)
                 pdf.multi_cell(width, 5, txt=diplomaDescription, border=False, align='C')
 
-                pdf.set_font(fontText, '', 14)
+                pdf.set_font(selectedFont, '', 14)
                 pdf.set_xy(180, 195)
                 pdf.cell(85, 15, txt=fechaTaller, border=False, align='C')
             
@@ -368,8 +415,6 @@ widget = QStackedWidget()
 screen1 = DiplomaFields()
 screen2 = SeleccionTemplate()
 screen3 = FileUpload()
-# screen4 = PreviewDiploma()
-# screen5 = SendMail()
 
 screens.append(screen1)
 screens.append(screen2)
